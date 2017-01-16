b6ad6c4cd34bde03c0d064aba6a5e0b926be1b35
Kevin Micol <kevin.d.micol@gmail.com>
2017-01-16 16:24:07 -0500

    Added Ua detection

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var http = require('http');
 var parser = require('ua-parser-js');
+var http = require('http')
+  , useragent = require('express-useragent');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var http = require('http');
+var parser = require('ua-parser-js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+
+
+http.createServer(function (req, res) {
+  // get user-agent header
+  var ua = parser(req.headers['user-agent']);
+
+  console.log(ua);
+  // write the result as response
+  res.end(JSON.stringify(ua, null, '  '));
+})
+.listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
